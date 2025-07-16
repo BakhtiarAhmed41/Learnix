eff50ca9931e57f5e05924f06ab96306a6ab7027
Dua-Sohail <sohaildua35@gmail.com>
2025-07-16 05:46:20 +0500

    getting ready for deployment

--- backend/study_assistant_project/urls.py
+++ backend/study_assistant_project/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False)),  # Add root redirect
     path('admin/', admin.site.urls),
     path('api/', include('study_assistant.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
